0776c6250ba48301f7e480e9dcc230526df0c029
victor-vas <victorvasconcelos88@gmail.com>
2021-02-01 11:13:23 -0300

    fixed env

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,3 +1,5 @@
-const api = process.env.API_URL ? process.env.API_URL : 'http://localhost:3333';
+const api = process.env.REACT_APP_API_URL
+  ? process.env.REACT_APP_API_URL
+  : 'http://localhost:3333';
 
 export default api;
